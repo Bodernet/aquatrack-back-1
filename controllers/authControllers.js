55c70f42c9606689cace096ccd304d88e94d7285
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-06-30 23:26:30 +0300

    зміна посилання на сінг

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -176,8 +176,8 @@ export async function verifyEmail(req, res, next) {
     }
 
     const redirectUrl =
-      `http://localhost:5173/signin?token=${token}` ||
-      `https://aquatrack-front-1.vercel.api/signin?token=${token}`;
+      `http://localhost:5173/signin` ||
+      `https://aquatrack-front-1.vercel.api/signin`;
 
     // const redirectUrl =
     //   `http://localhost:5173/tracker?token=${token}` ||
@@ -438,8 +438,8 @@ export const googleRedirect = async (req, res, next) => {
     await tokenServices.saveToken(user._id, refreshToken);
     await User.findByIdAndUpdate(user._id, { token }, { new: true });
     const redirectUrl =
-      `http://localhost:5173/signin?token=${token}` ||
-      `https://aquatrack-front-1.vercel.api/signin?token=${token}`;
+      `http://localhost:5173/tracker` ||
+      `https://aquatrack-front-1.vercel.api/tracker`;
     res
       .cookie("refreshToken", refreshToken, cookieConfig)
       .redirect(redirectUrl);
