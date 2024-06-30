c6062c11c6369ec662bd2de0b1082d575eea5e21
Bodernet <142262665+Bodernet@users.noreply.github.com>
2024-06-30 16:47:10 +0300

    fix midlware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -14,29 +14,23 @@ async function authMiddleware(req, res, next) {
       .status(401)
       .send({ message: "Not authorized! Missing or invalid token." });
   }
-  jwt.verify(token, process.env.JWT_SECRET, async (error, decode) => {
-    if (error) {
-      return res
-        .status(401)
-        .json({ message: "Not authorized! Invalid token!" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user || user.token !== token) {
+      return res.status(401).json({
+        message: "Not authorized! User not found or token mismatch!",
+      });
     }
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decode.id);
-      if (!user || user.token !== token) {
-        return res.status(401).json({
-          message: "Not authorized! User not found or token mismatch!",
-        });
-      }
 
-      req.user = { id: user._id, email: user.email };
-      next();
-    } catch (error) {
-      console.log("Authorization error:", error);
-      return res
-        .status(401)
-        .json({ message: "Server error during authorization!" });
-    }
-  });
+    req.user = { id: user._id, email: user.email };
+    next();
+  } catch (error) {
+    console.log("Authorization error:", error);
+    return res
+      .status(401)
+      .json({ message: "Server error during authorization!" });
+  }
 }
+
 export default authMiddleware;
